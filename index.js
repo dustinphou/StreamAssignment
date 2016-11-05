8b909e7a2ebc7016dc08bc7eda01067f49aa8b76
dustinphou <dustinphou@sjsu.edu>
2016-11-05 11:59:23 -0700

    Added parsing for console argument

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 var fs = require('fs')
 var Transform = require('stream').Transform
 var inherits = require("util").inherits
+var program = require('commander')
 
-function PatternMatch()
-{
-	Transform.call(this, {objectMode: true});
+function PatternMatch(arg){
+	Transform.call(this, {objectMode: true})
 }
-
-console.log("Enter character to filter out.")
 
 inherits(PatternMatch, Transform)
 
 PatternMatch.prototype._transform = function (chunk, encoding, getNextChunk){
-
 var string = chunk.toString()
-var result = string.split('.')
-
+var result = string.split(program.pattern)
+if(result[result.length-1] == '')	//cuts off the null result when '.' is argument
+{
+	result.length -= 1
+}
 this.push(result)
 getNextChunk()	//callback just causes stream to proceed to next chunk, does not need to be defined
-
 }
 
 PatternMatch.prototype._flush = function (flushCompleted) {
-	flushCompleted()
+	flushCompleted() //same as above callback
 }
 
-//add console functionality after streams are working
-//program.option('-p, --pattern <pattern>', 'Input Pattern such as . ,').parse(process.argv);
+program.option('-p, --pattern <pattern>', 'Input Pattern such as . ,').parse(process.argv);
 
-var instream = fs.createReadStream("input-sensor.txt");
+var instream = fs.createReadStream("input-sensor.txt")
 
-var patternStream = instream.pipe( new PatternMatch());
+var patternStream = instream.pipe(new PatternMatch())
 
 patternStream.on('data', function(data) {
 	console.log(data)
